1c7b90fdb6d4c5ded1c3890bd85fc188c7b3b8a1
Ayesha Jamil <ge73xij@mytum.de>
2020-06-17 14:46:07 +0200

    Add homer name and color in signin page

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 
-import {
-    TotalDocuments,
-    TotalGlobalDocuments,
-    TotalUsers
-} from './components';
 
 const useStyles = makeStyles(theme => ({
     root: {
