7437a727a05bad99ab2e3da65651133abc1dabe2
Ayesha Jamil <ge73xij@mytum.de>
2020-07-09 13:31:04 +0200

    admin changes

---
+++ src/service/VideoService.js
@@ -0,0 +1,32 @@
+import HttpService from './HttpService';
+
+const baseURL = process.env.REACT_APP_BACKEND_API_URL + '/video';
+
+const jwtDecode = require('jwt-decode');
+
+const saveVideo = (VideoName, keywords, VideoURL, Duration, User) => {
+    const VideoObject = {
+        videoTitle: VideoName,
+        keywords: keywords,
+        videoUrl: VideoURL,
+        duration: Duration,
+        uploader: User
+    };
+
+    return HttpService.post(baseURL + '/SaveVideo', VideoObject)
+        .then(resp => {
+            if (resp.status === 200) {
+                return resp.json()
+                    .then(json => {
+                        return Promise.resolve(resp);
+                    });
+            } else {
+                return Promise.reject(resp);
+            }
+        });
+};
+
+
+export default {
+    saveVideo
+};
