4825193d8283b6d94f088d6f9461b18c4645affb
KiranTauqir <ge68pon@mytum.de>
2020-07-12 23:02:06 +0200

    getting URL based on keywords

---
+++ src/views/SearchResultVideo/SearchResultVideo.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router-dom';
+import { VideoService } from '../../service';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(4)
+    }
+}));
+
+
+
+
+const SearchResultVideo = props => {
+    const classes = useStyles();
+    let location = useLocation();
+    let history = useHistory();
+    const onSearchClick = () => {
+
+
+        VideoService.GetVideoByTag(location.state.TagName)
+            .then(data => {
+                console.log(data);
+                console.log(data.videoTitle)
+                console.log( data.videoUrl)
+
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+
+    };
+    return (
+        <div className={classes.root}>
+            this is search result page
+            {onSearchClick()}
+        </div>
+    );
+};
+
+export default SearchResultVideo;
