e7c5449f50d80be4b3b9ea746722fde1470a48dd
KiranTauqir <ge68pon@mytum.de>
2020-07-13 10:43:35 +0200

    getting URL based on keywords

--- src/views/SearchResultVideo/SearchResultVideo.js
+++ src/views/SearchResultVideo/SearchResultVideo.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 import { useHistory, useLocation } from 'react-router-dom';
 import { VideoService } from '../../service';
+import ReactPlayer from "react-player"
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,19 +13,23 @@ const useStyles = makeStyles(theme => ({
 
 
 
-let DocumentList=[];
+let DocumentList={};
 const SearchResultVideo = props => {
     const classes = useStyles();
     let location = useLocation();
     let history = useHistory();
+    const [showVideo, setshowVideo] = React.useState(false);
 
 
     const onSearchClick = () => {
-
-
         VideoService.GetVideoByTag(location.state.TagName)
             .then(data => {
                 DocumentList=data;
+
+                setshowVideo(true);
+                // Object.keys(DocumentList).map((position, index) => {
+                //     console.log( DocumentList[position][index].videoTitle)
+                // })
                 console.log(DocumentList)
             })
             .catch((e) => {
@@ -34,8 +39,31 @@ const SearchResultVideo = props => {
     };
     return (
         <div className={classes.root}>
-            this is search result page
+
             {onSearchClick()}
+
+
+
+
+            {Object.keys(DocumentList).map((item,index) => {
+            //{DocumentList.Video.map((Video, index)  => {
+                return (
+                    showVideo ? (
+                        <div  >
+                            this is wierd
+                            {DocumentList[item][index].videoTitle}
+                            {DocumentList[item][index].videoTitle}
+                            {/*<ReactPlayer*/}
+                            {/*    url="https://www.youtube.com/watch?v=ug50zmP9I7s"*/}
+                            {/*/>*/}
+                        </div>
+                    ) : <div>
+                        this is false
+                    </div>
+                );
+            })}
+
+
         </div>
     );
 };
