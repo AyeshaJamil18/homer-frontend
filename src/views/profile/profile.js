d88805e9a948c6a2f862d5bb3ce3c37da7f26f8d
derfesl <m.fesl@tum.de>
2020-07-14 16:17:30 +0200

    populate groups counter

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Link as RouterLink, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { Breadcrumbs } from '@material-ui/core';
+import { UserService } from '../../service';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,6 +20,19 @@ const useStyles = makeStyles(theme => ({
 
 const Profile = props => {
     const classes = useStyles();
+    const [countFriends, setCountFriends] = React.useState(0);
+    const [countGroups, setCountGroups] = React.useState(0);
+
+    useEffect(() => {
+        /*UserService.friends()
+            .then(friends => { 
+                setCountFriends(friends.length);
+            }).catch(e => console.error(e));*/
+        UserService.groups()
+            .then(groups => { 
+                setCountGroups(groups.member.length);
+            }).catch(e => console.error(e));
+    }, [])
 
     return (
         <Grid container spacing={3} className={classes.root}>
@@ -30,8 +44,8 @@ const Profile = props => {
             <Grid item xs={5} sm={4} md={3} lg={2}>
                 <Card>
                     <CardContent style={{textAlign: 'center'}}>
-                        <Typography variant="h1" color="primary"> 21 </Typography>
-                        <Typography variant="h5"> Friends </Typography>
+                        <Typography variant="h1" color="primary"> {countFriends} </Typography>
+                        <Typography variant="h5"> { countFriends == 1 ? 'Friend' : 'Friends' } </Typography>
                     </CardContent>
                     <CardActions>
                         <Button size="small" onClick={() => props.history.push('/friends')}> Manage </Button>
@@ -41,8 +55,8 @@ const Profile = props => {
             <Grid item xs={5} sm={4} md={3} lg={2}>
                 <Card>
                     <CardContent style={{textAlign: 'center'}}>
-                        <Typography variant="h1" color="primary"> 4 </Typography>
-                        <Typography variant="h5"> Groups </Typography>
+                        <Typography variant="h1" color="primary"> {countGroups} </Typography>
+                        <Typography variant="h5"> { countGroups == 1 ? 'Group' : 'Groups' } </Typography>
                     </CardContent>
                     <CardActions>
                         <Button size="small" onClick={() => props.history.push('/groups')}> Manage </Button>
