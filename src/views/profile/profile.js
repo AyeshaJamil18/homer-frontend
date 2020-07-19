491a9f57df1e540fa5b83963c1df39283502c916
KiranTauqir <ge68pon@mytum.de>
2020-07-19 16:18:21 +0200

    Added FB share

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -17,8 +17,19 @@ import {
     DialogContentText,
     DialogTitle,
 } from '@material-ui/core';
-import { UserService } from '../../service';
+import { PlaylistService, UserService } from '../../service';
 import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import {
+
+    FacebookShareButton,
+    FacebookIcon
+
+} from 'react-share';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -28,12 +39,28 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Profile = props => {
-  //  const alert =useAlert();
+    //  const alert =useAlert();
     const classes = useStyles();
     const [countFriends, setCountFriends] = React.useState(0);
     const [countGroups, setCountGroups] = React.useState(0);
     const [open, setOpen] = React.useState(false);
     const [selectedPlaylistName, setSelectedPlaylistName] = React.useState('');
+    const [open_ViewAllPlaylists, setOpen_ViewAllPlaylists] = React.useState(false);
+    const [documentList, setDocumentList] = React.useState([]);
+
+    const GetUSerPlaylist = () =>{
+        PlaylistService.GetPlaylist()
+            .then(data => {
+                console.log(data);
+                console.log(data.docs);
+                setDocumentList(data.docs);
+                console.log(documentList);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
 
     const handleClose = () => {
         setOpen(false);
@@ -41,6 +68,26 @@ const Profile = props => {
     const handleClickOpen = () => {
         setOpen(true);
     };
+
+
+
+    const handleClickOpen_ViewAllPlaylists = () => {
+        console.log('in handle click open')
+        GetUSerPlaylist();
+        console.log('after func');
+        setOpen_ViewAllPlaylists(true);
+    };
+
+
+    const handleClose_ViewAllPlaylists = () => {
+        setOpen_ViewAllPlaylists(false);
+    };
+
+
+
+
+
+
 
     const SavePlaylist = () => {
         console.log(selectedPlaylistName);
@@ -55,7 +102,7 @@ const Profile = props => {
 
     useEffect(() => {
         UserService.friends()
-            .then(friends => { 
+            .then(friends => {
                 setCountFriends(friends.length);
             }).catch(e => console.error(e));
         UserService.groups()
@@ -64,9 +111,63 @@ const Profile = props => {
             }).catch(e => console.error(e));
     }, [])
 
+    const shareUrl = 'http://localhost:8000/dashboard';
+    const title = 'HOMER';
 
     return (
         <div>
+
+            <Dialog
+                autoDetectWindowHeight={false}
+                autoDetectWindowWidth={false}
+                aria-labelledby="form-dialog-title"
+                onClose={handleClose_ViewAllPlaylists}
+                open={open_ViewAllPlaylists}
+            >
+                <DialogTitle id="form-dialog-title">Your Playlist</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Select the playlists you want to share:
+                    </DialogContentText>
+                    <Grid>
+                        <List>
+                            { documentList.map((document)  => (
+                                <ListItem>
+                                    <ListItemText
+                                        primary = {document.title}
+                                    />
+                                    <ListItemSecondaryAction>
+                                        <Button  > Share </Button>
+                                        <FacebookShareButton
+                                            url={shareUrl}
+                                            quote={title}
+                                            className="Demo__some-network__share-button"
+                                        >
+                                            <FacebookIcon size={32} round />
+                                        </FacebookShareButton>
+
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            ))
+                            }
+                        </List>
+                    </Grid>
+
+                    {/* {selectedValue == 'None'} */}
+
+
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        color="primary"
+                        onClick={handleClose_ViewAllPlaylists}
+                    >
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+
             <Dialog
                 aria-labelledby="form-dialog-title"
                 onClose={handleClose}
@@ -87,12 +188,9 @@ const Profile = props => {
                             //value={selectedValueMaskRange}
                             onChange={handlePlaylistName}
                             value={selectedPlaylistName}
-
                         />
                         <span className={classes.spacer}/>
                     </Grid>
-
-
                 </DialogContent>
                 <DialogActions>
                     <Button
@@ -126,10 +224,8 @@ const Profile = props => {
                 </Grid>
                 <Grid
                     item
-                    lg={2}
                     md={3}
-                    sm={4}
-                    xs={5}
+                    xs={2}
                 >
                     <Card>
                         <CardContent style={{textAlign: 'center'}}>
@@ -149,10 +245,8 @@ const Profile = props => {
                 </Grid>
                 <Grid
                     item
-                    lg={2}
                     md={3}
-                    sm={4}
-                    xs={5}
+                    xs={2}
                 >
                     <Card>
                         <CardContent style={{textAlign: 'center'}}>
@@ -170,6 +264,8 @@ const Profile = props => {
                         </CardActions>
                     </Card>
                 </Grid>
+
+
                 <Grid
                     item
                     md={7}
@@ -183,13 +279,21 @@ const Profile = props => {
                                 gutterBottom
                                 variant="h5"
                             >
-                            PLAYLIST
+                                PLAYLIST
                             </Typography>
                             <CardActions>
                                 <Grid allign = "row" >
-                                    <Button size="small">Share Playlist</Button>
+                                    <Button    onClick={() => handleClickOpen_ViewAllPlaylists()}
+                                               size="small">Share Playlist</Button>
+
+
+
+
+
+
+
                                     <Button
-                                        onClick={() => handleClickOpen()} 
+                                        onClick={() => handleClickOpen()}
                                         size="small"
                                     >Create New Playlist</Button>
                                 </Grid>
@@ -210,7 +314,7 @@ const Profile = props => {
                                 gutterBottom
                                 variant="h5"
                             >
-                            SETTINGS
+                                SETTINGS
                             </Typography>
 
                         </CardContent>
